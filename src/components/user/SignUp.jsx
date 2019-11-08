94cf9cbe2f3cde27da636dbe6862b8fd7ef57187
Nyannazz <kantemir.imam@gmail.com>
2019-11-08 10:48:39 +0100

    sign up status added

--- src/components/user/SignUp.jsx
+++ src/components/user/SignUp.jsx
@@ -7,27 +7,34 @@ const SignUp = ({ signedUp }) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [passwordRe, setPasswordRe] = useState("");
-
+    const [signUpStatus, setStatus]=useState("SIGN UP");
 
     const signUp = (event) => {
         event.preventDefault();
-        if (this.validateState) {
+        if (password===passwordRe) {
             const formData = new FormData();
-            formData.append("name", this.state.name);
-            formData.append("email", this.state.email);
-            formData.append("password", this.state.password);
+            formData.append("name", name);
+            formData.append("email", email);
+            formData.append("password", password);
             axios.post(`${BASEURL}/signup`, formData).then(response => {
-                signedUp(1);
+                /* signedUp(1); */
+                signUpSucces();
+                console.log(response)
             }).catch(error => {
                 window.alert("failure")
                 console.log(error)
             })
         }
-
     }
+
+    const signUpSucces=()=>{
+        setStatus("SUCCESFULLY SIGNED UP")
+        setTimeout(()=>setStatus("SIGN UP"),8000);
+    }
+
     return (
         <form onSubmit={signUp} className={'logSignForm'}>
-            <h1>SIGN UP</h1>
+            <h1>{signUpStatus}</h1>
             <input value={name} onChange={(event) => setName(event.target.value)} name='name' placeholder='user name' type="text" />
             <input value={email} onChange={(event) => setEmail(event.target.value)} name='email' placeholder='email' type="email" />
             <input value={password} onChange={(event) => setPassword(event.target.value)} name='password' placeholder='password' type="password" />
