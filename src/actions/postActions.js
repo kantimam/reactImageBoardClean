dd330275213225a12dfdd9677514317acabbc770
Nyannazz <kantemir.imam@gmail.com>
2019-12-05 20:40:06 +0100

    added actions for pagination

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -5,7 +5,17 @@ import {
   SEARCH_POSTS,
   GET_FAVORITE_POSTS,
   GET_USER_POSTS,
-  SET_PREVIEW
+  SET_PREVIEW,
+  NEXT_NEW_POSTS,
+  PREV_NEW_POSTS,
+  NEXT_SEARCH_POSTS,
+  PREV_SEARCH_POSTS,
+  NEXT_POPULAR_POSTS,
+  PREV_POPULAR_POSTS,
+  NEXT_FAVORITE_POSTS,
+  PREV_FAVORITE_POSTS,
+  NEXT_USER_POSTS,
+  PREV_USER_POSTS
 } from './types';
 import axios from 'axios';
 const BASEURL = process.env.REACT_APP_BE_URL
@@ -14,25 +24,114 @@ const BASEURL = process.env.REACT_APP_BE_URL
 
 export const getPostWithPreview = (id, bucket) => {
   return (dispatch, getState) => {
-    const {posts} = getState();
+    const {
+      posts
+    } = getState();
     if (posts[bucket]) {
-      let postPreview = [];
+      dispatch(getPost(id));
       if (posts[bucket].data && posts[bucket].data.length > 0) {
         const currentPosts = posts[bucket].data;
         const postId = currentPosts.findIndex((e) => e.id == id);
-        postPreview = [currentPosts[postId - 2], currentPosts[postId - 1], currentPosts[postId], currentPosts[postId + 1], currentPosts[postId + 2]];
+        console.log(currentPosts)
+        if (postId < 2) dispatch(getPrevPage(posts[bucket].prev_page_url, bucket))
+        if (postId > currentPosts.length - 2) dispatch(getNextPage(posts[bucket].next_page_url, bucket))
+
+        console.log(getState())
+        dispatch(getPreview(currentPosts, postId))
+
       }
-      dispatch({
-        type: SET_PREVIEW,
-        payload: postPreview,
-      })
-      if(bucket==="search") return dispatch(searchPosts(id));
-      dispatch(getPost(id));
-    }else{
+
+    } else {
       console.log("please check your route")
     }
   }
 }
+
+const getPreview = (currentPosts, postId) => dispatch => {
+  console.log("preview was called")
+  let postPreview = [currentPosts[postId - 2], currentPosts[postId - 1], currentPosts[postId], currentPosts[postId + 1], currentPosts[postId + 2]];
+  dispatch({
+    type: SET_PREVIEW,
+    payload: postPreview,
+  })
+}
+
+export const getNextPage = (url, bucket) => {
+  console.log("get next page")
+  if (url) {
+    axios(url)
+      .then(res => {
+          dispatchNewer(bucket, res.data)
+        }
+      )
+  }
+}
+
+
+export const getPrevPage = (url, bucket) => {
+  if (url) {
+    axios(url)
+      .then(res => {
+          dispatchOlder(bucket, res.data)
+        }
+      )
+  }
+
+}
+
+const dispatchNewer = (bucket, data) => dispatch => {
+  let type;
+  switch (bucket) {
+    case "new":
+      type = PREV_NEW_POSTS;
+      break;
+    case "popular":
+      type = PREV_POPULAR_POSTS;
+      break;
+    case "search":
+      type = PREV_SEARCH_POSTS;
+      break;
+    case "user":
+      type = PREV_USER_POSTS;
+      break;
+    case "favorite":
+      type = PREV_FAVORITE_POSTS;
+      break;
+    default:
+      return;
+  }
+  dispatch({
+    type: type,
+    payload: data
+  })
+}
+const dispatchOlder = (bucket, data) => dispatch => {
+  let type;
+  switch (bucket) {
+    case "new":
+      type = NEXT_NEW_POSTS;
+      break;
+    case "popular":
+      type = NEXT_POPULAR_POSTS;
+      break;
+    case "search":
+      type = NEXT_SEARCH_POSTS;
+      break;
+    case "user":
+      type = NEXT_USER_POSTS;
+      break;
+    case "favorite":
+      type = NEXT_FAVORITE_POSTS;
+      break;
+    default:
+      return;
+  }
+  dispatch({
+    type: type,
+    payload: data
+  })
+}
+
 
 export const getPost = (id) => dispatch => {
   console.log("get was called")
@@ -43,25 +142,6 @@ export const getPost = (id) => dispatch => {
         payload: res.data
       })
     )
-
-
-
-  /* getCurrentPost=(id)=>{
-    this.getPost(id, 'posts')
-    .then(post=>{
-      this.setState({
-        post:post,
-        favorite: post.users_with_favorite,
-        vote:  post.vote,
-        rating: post.rating,
-        postId:this.props.match.params.postId,
-        prev: post.prev || [],
-        next: post.next || []
-      })
-        this.serverRating=post.rating
-      }).catch(error=>this.handleError(error))
-    
-  } */
 }
 
 
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,6 +1,24 @@
-import {GET_POST, GET_NEW_POSTS, GET_POPULAR_POSTS ,SEARCH_POSTS, GET_USER_POSTS, GET_FAVORITE_POSTS, SET_PREVIEW} from '../actions/types';
+import {
+    GET_POST,
+    GET_NEW_POSTS,
+    GET_POPULAR_POSTS,
+    SEARCH_POSTS,
+    GET_USER_POSTS,
+    GET_FAVORITE_POSTS,
+    SET_PREVIEW,
+    NEXT_NEW_POSTS,
+    PREV_NEW_POSTS,
+    NEXT_SEARCH_POSTS,
+    PREV_SEARCH_POSTS,
+    NEXT_POPULAR_POSTS,
+    PREV_POPULAR_POSTS,
+    NEXT_FAVORITE_POSTS,
+    PREV_FAVORITE_POSTS,
+    NEXT_USER_POSTS,
+    PREV_USER_POSTS
+} from '../actions/types';
 
-const initialState={
+const initialState = {
     user: [],
     search: [],
     new: [],
@@ -9,45 +27,50 @@ const initialState={
     preview: []
 }
 
-export default function(state=initialState, action){
-    switch(action.type){
+export default function (state = initialState, action) {
+    switch (action.type) {
         case GET_POST:
             return {
                 ...state,
                 current: action.payload
             }
-        case GET_NEW_POSTS:
-            return {
-                ...state,
-                new: action.payload
-            }
-        case GET_POPULAR_POSTS:
-            return {
-                ...state,
-                popular: action.payload
-            }
-        case SEARCH_POSTS:
-            console.log(action)
-            return {
-                ...state,
-                search: action.payload
-            }
-        case GET_USER_POSTS:
-            return {
-                ...state,
-                user: action.payload
-            }
-        case GET_FAVORITE_POSTS:
-            return {
-                ...state,
-                favorite: action.payload
-            }
-        case SET_PREVIEW:
-            return {
-                ...state,
-                preview: action.payload,
-            }
-        default:
-            return state;
+            case GET_NEW_POSTS:
+                return {
+                    ...state,
+                    new: action.payload
+                }
+                case GET_POPULAR_POSTS:
+                    return {
+                        ...state,
+                        popular: action.payload
+                    }
+                    case SEARCH_POSTS:
+                        console.log(action)
+                        return {
+                            ...state,
+                            search: action.payload
+                        }
+                        case GET_USER_POSTS:
+                            return {
+                                ...state,
+                                user: action.payload
+                            }
+                            case GET_FAVORITE_POSTS:
+                                return {
+                                    ...state,
+                                    favorite: action.payload
+                                }
+                                case SET_PREVIEW:
+                                    return {
+                                        ...state,
+                                        preview: action.payload,
+                                    }
+                                case NEXT_NEW_POSTS:
+                                    return {
+                                        ...state,
+                                        new: {...action.payload, data: [...state.new.data, ...action.payload.data]}
+                                    }
+                                    default:
+                                        return state;
     }
 }
